1146d54cbf6d3a463768c18e58b99a15374b4416
Patrick Roserie <patrick@roserie.me>
2022-03-11 20:28:14 -0500

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "s3" {
-    bucket = var.backend_bucket
-    key    = var.backend_key
-    region = var.aws_region
   }
 }
 
@@ -13,7 +10,7 @@ provider "aws" {
 data "aws_iam_policy_document" "linktree" {
   version = "2012-10-17"
   statement {
-    sid = "PublicReadGetObject"
+    sid    = "PublicReadGetObject"
     effect = "Allow"
     principals {
       identifiers = ["*"]
@@ -32,27 +29,27 @@ resource "aws_s3_bucket_policy" "linktree" {
 }
 
 resource "aws_s3_object" "linktree_html" {
-  bucket = aws_s3_bucket.assets.bucket
-  key    = "index.html"
-  source = "source/index.html"
+  bucket       = aws_s3_bucket.assets.bucket
+  key          = "index.html"
+  source       = "source/index.html"
   content_type = "text/html"
-  source_hash = filemd5("source/index.html")
+  source_hash  = filemd5("source/index.html")
 }
 
 resource "aws_s3_object" "linktree_css" {
-  bucket = aws_s3_bucket.assets.bucket
-  key    = "style.css"
-  source = "source/style.css"
+  bucket       = aws_s3_bucket.assets.bucket
+  key          = "style.css"
+  source       = "source/style.css"
   content_type = "text/css"
-  source_hash = filemd5("source/style.css")
+  source_hash  = filemd5("source/style.css")
 }
 
 resource "aws_s3_bucket" "assets" {
   bucket = var.aws_bucket
   tags = {
-    deployed_with_terraform     = true
-    deployed_from_github = true
-    description = "linktree s3 bucket"
+    deployed_with_terraform = true
+    deployed_from_github    = true
+    description             = "linktree s3 bucket"
   }
 }
 
@@ -75,5 +72,5 @@ resource "aws_s3_bucket_cors_configuration" "linktree" {
 
 resource "aws_s3_bucket_acl" "linktree_acl" {
   bucket = aws_s3_bucket.assets.bucket
-  acl = "public-read"
+  acl    = "public-read"
 }
