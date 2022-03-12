a43101540e8052f09b83fde53275d7914f271b54
Patrick Roserie <patrick@roserie.me>
2022-03-12 15:54:56 -0500

    remove comments

--- main.tf
+++ main.tf
@@ -21,40 +21,6 @@ data "aws_iam_policy_document" "linktree" {
       "arn:aws:s3:::${var.aws_bucket}/*"
     ]
   }
-  #  statement {
-  #    sid     = "DenyNonCloudFlare"
-  #    effect  = "Deny"
-  #    actions = ["s3:*"]
-  #    resources = [
-  #      "arn:aws:s3:::${var.aws_bucket}",
-  #      "arn:aws:s3:::${var.aws_bucket}/*"
-  #    ]
-  #    condition {
-  #      test = "NotIpAddress"
-  #      values = [
-  #        "173.245.48.0/20",
-  #        "103.21.244.0/22",
-  #        "103.22.200.0/22",
-  #        "103.31.4.0/22",
-  #        "141.101.64.0/18",
-  #        "108.162.192.0/18",
-  #        "190.93.240.0/20",
-  #        "188.114.96.0/20",
-  #        "197.234.240.0/22",
-  #        "198.41.128.0/17",
-  #        "162.158.0.0/15",
-  #        "104.16.0.0/13",
-  #        "104.24.0.0/14",
-  #        "172.64.0.0/13",
-  #        "131.0.72.0/22"
-  #      ]
-  #      variable = "aws:VpcSourceIp"
-  #    }
-  #    principals {
-  #      identifiers = ["*"]
-  #      type        = "*"
-  #    }
-  #  }
 }
 
 resource "aws_s3_bucket_policy" "linktree" {
